945ee6093de9111b65f627941c84f18f80a3c42d
kroneyen <krone.huang@gmail.com>
2021-10-21 10:45:23 +0800

    remove comment

--- report/stock_month_report.py
+++ report/stock_month_report.py
@@ -50,7 +50,6 @@ def monthly_report():
      ###上櫃
      url_1 ='https://mops.twse.com.tw/nas/t21/otc/t21sc03_' + str(report_day.year - 1911) + '_' + str(report_day.month) +'_0.html'
      today_week = datetime.date.today().strftime("%w")
-     #mail_time = "21:00:00"
      
      url_list = [url,url_1]
      df_report = pd.DataFrame()
@@ -64,11 +63,9 @@ def monthly_report():
            df_table = pd.DataFrame(df_table) ##redefine DataFrame
            df_table = df_table.iloc[:-1,[0,1,2,4,6,7,8,9]]   ###row -1 去除合計 ; columns : 公司代號 公司名稱 當月營收 去年當月營收 去年同月增減(%)  當月累計營收 去年累計營收 前期比較增減(%)
            df_report = pd.concat([df_report,df_table],ignore_index = True)
-     #print(df_report.columns)    
      df_report.columns=["公司代號","公司名稱","當月營收","去年當月營收", "YoY(%)" , "當月累計營收","去年累計營收", "累計YoY(%)"]
      com_lists = get_redis_data()
      match_row = df_report[df_report['公司代號'].isin(com_lists)]
-     #match_row = match_row.sort_values(by=['累計YoY(%)'],ascending = False,ignore_index = True) 
 
      df_report = pd.DataFrame()
      df_report = match_row.iloc[:,[0,1,4,7]].copy()       
@@ -78,25 +75,17 @@ def monthly_report():
      df_report['去年累計(千萬)'] = round(match_row['去年累計營收']/ 1000000,2)    
      df_report = df_report.iloc[:,[0,1,4,5,2,6,7,3]]
      df_report = df_report.sort_values(by=['累計YoY(%)'],ascending = False,ignore_index = True)
-     #return match_row.iloc[:,[0,1,8,9,4,10,11,7]]
-     #return match_row.iloc[:,[0,1,8,9,4,10,11,7]]
      return df_report,report_day
 
 report ,report_day = monthly_report()
-#report = report.to_markdown(index= 0)
-#report = report.to_html()
-#report = tableize(report)
-#print(report_day)
 mail_month =str(report_day.month)
 if time.strftime("%H:%M:%S", time.localtime()) > mail_time :
     if len(report) > 0 :
        body = report.to_html(escape=False)
-       #send_mail.send_email('stock_month_report',body)
        send_mail.send_email('stock_month_%s_report' % mail_month ,body)
 
 else :
     report = report.to_markdown(index= 0)
-    #print(report.to_string(index=False))
     print('stock_month_%s_report' % mail_month)
     print(report)
  
