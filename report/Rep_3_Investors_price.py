1c0ee3771d58d5b069045dfee2f055b16fca7405
kroneyen <krone.huang@gmail.com>
2023-04-19 16:22:01 +0800

    add matplotlib / fake_useragent

--- report/Rep_3_Investors_price.py
+++ report/Rep_3_Investors_price.py
@@ -11,7 +11,25 @@ import send_mail
 from IPython.display import display, clear_output
 from io import StringIO
 from pymongo import MongoClient
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.font_manager import fontManager
+from fake_useragent import UserAgent
+import del_png
 
+
+
+# 改style要在改font之前
+plt.style.use('seaborn')
+fontManager.addfont('images/TaipeiSansTCBeta-Regular.ttf')
+mpl.rc('font', family='Taipei Sans TC Beta')
+
+
+
+
+
+user_agent = UserAgent()
 
 mail_time = "18:00:00"
 #mail_time = "09:00:00"
@@ -19,8 +37,8 @@ mail_time = "18:00:00"
 date_sii = datetime.date.today().strftime('%Y%m%d')
 date_otc = str(int(datetime.date.today().strftime('%Y')) - 1911)  +  datetime.date.today().strftime('/%m/%d')
 
-#date_sii='20230322'
-#date_otc='112/03/22'
+#date_sii='20230418'
+#date_otc='112/04/18'
 
 match_row = pd.DataFrame()
 
@@ -108,6 +126,13 @@ def read_mongo_db(_db,_collection,dicct):
     #return collection.find({_code:_qq},{"code":1,"name":0,"_id":0,"last_modify":0})
     return collection.find(dicct,{"code": 1,"name": 1,"_id": 0})
 
+def read_mongo_db(_db,_collection,dicct,_columns):
+    db = c[_db] ## database
+    collection = db[_collection] ## collection 
+    return collection.find(dicct,_columns)
+
+
+
 
 def atlas_read_mongo_db(_db,_collection,dicct,_columns):
     db = conn[_db] ## database
@@ -130,7 +155,11 @@ def read_aggregate_mongo_db(_db,_collection,dicct):
 
 
 
+
+
+
 def Rep_3_Investors(date_sii,date_otc,com_lists) : 
+
 
 
   #url_sii ='https://www.twse.com.tw/fund/T86?response=html&date='+ date_sii +'&selectType=ALL'
@@ -144,7 +173,7 @@ def Rep_3_Investors(date_sii,date_otc,com_lists) :
   
   for url in url_list :
      
-     r = requests.get(url)
+     r = requests.get(url , headers={ 'user-agent': user_agent.random }) 
      r.encoding = 'utf8'
      df = pd.read_html(r.text,thousands=",")[0]
      df_len = len(df.columns)
@@ -206,7 +235,7 @@ def Rep_price(date_sii,date_otc,com_lists):
        u_index = 0
        dfs = pd.DataFrame()
        for url in url_list :
-              r = requests.get(url)
+              r = requests.get(url ,  headers={ 'user-agent': user_agent.random })
               r.encoding = 'utf8'
               if u_index == 0 :
                 df = pd.read_html(r.text,thousands=",")[8] ## []list to pandas
@@ -245,7 +274,6 @@ def Rep_price(date_sii,date_otc,com_lists):
 
 
 
-
 df_s = pd.DataFrame()
 
 #com_lists = get_redis_data("com_list") ## get  redis data
@@ -268,6 +296,16 @@ except :
 
 
 
+
+### del images/*.png
+del_png.del_images()
+
+
+
+### call function
+
+
+
 df_Rep_3_Investors = Rep_3_Investors(date_sii,date_otc,com_lists)
 df_Rep_price = Rep_price(date_sii,date_otc,com_lists)
 
@@ -281,7 +319,8 @@ match_row = df_s.sort_values(by=['漲跌(+/-)'],ascending=False,ignore_index= Tr
 local_mongo=[]
 local_redis=[]
 
-local_mongo = read_mongo_db('stock','com_list',dictt)
+local_mongo = read_mongo_db('stock','com_list',{},{"code": 1,"name": 1,"_id": 0})
+
 local_redis = get_redis_data('com_list','lrange',0,-1) 
      
 if com_lists != local_mongo : 
@@ -426,6 +465,65 @@ def  get_auth_stock(_db,_collection,com_lists):
 
 
 
+def get_mongo_last_date(cal_day):
+   ### mongo query for last ? days
+   dictt_set = [ {"$group": { "_id" : { "$toInt" : "$last_modify" } }},{"$sort" : {"_id" :-1}} , { "$limit" : cal_day},{"$sort" : {"_id" :1}} , { "$limit" :1}]
+
+   ### mongo dict data
+
+   set_doc =  read_aggregate_mongo_db('stock','Rep_3_Investors',dictt_set)
+
+   ### for lists  get cal date 
+   for idx in set_doc:
+
+       idx_date = idx.get("_id")
+
+   #set_date = str(idx_date)
+   return str(idx_date)
+
+
+
+
+def plot_Rep_3_Investors_price(match_row) :
+
+   dfs = pd.DataFrame()
+   
+   ### match_row  code con_days
+   cal_day = match_row.iloc[:,[1]].astype('int64').abs().max() ###max con_days
+
+   last_modify = get_mongo_last_date(int(cal_day))
+   ### dataframe to list 
+   com_lists = match_row.iloc[:,[0]].reset_index(drop=True).squeeze()
+   
+   for idx in com_lists :
+
+      altas_mydoc = read_mongo_db('stock','Rep_3_Investors',{'code': idx ,'last_modify': {'$gte' : last_modify}},{'_id':0})
+
+      df = pd.DataFrame(list(altas_mydoc))
+
+      df = df.iloc[:,[0,1,5,6]]
+
+      rest_df = pd.DataFrame(data = df['total'].values,columns=[df['code'][0]+'_'+ df['name'][0]] , index=df['last_modify'])
+
+      dfs = pd.concat([dfs,rest_df],axis=1)
+
+
+   ### data to int for plot  
+   records = dfs.astype('int64')
+
+   ### 5 rows for each paint  
+   for idx in range(0,len(records.columns),5):
+
+     idx_records = records.iloc[ : , idx:idx+5 ]
+
+     idx_records.plot(y = idx_records.columns)
+     #idx_records.plot.line(y = idx_records.columns, figsize=(10,6))
+     #idx_records.plot.bar(y = idx_records.columns, figsize=(10,6))
+     ### del png with crontab jobs
+     plt.savefig('./images/image_'+ str(idx) +'_'+ str(idx+4) +'.png' )
+
+
+
 
 
 ### change column name for merge
@@ -441,21 +539,34 @@ match_row =pd.merge( pd.merge(match_row,df_auth_stock,on = ['code'],how='left'),
 
 match_row['1day%'] = round(round(match_row['total']/match_row['auth_stock'],4)*100 ,2)
 match_row['con_days%'] = round(round(match_row['total_values']/match_row['auth_stock'],4)*100 ,2)
+match_row = match_row.sort_values(by=['con_days%'],ascending = False)
 
 ### select column
 match_row = match_row.iloc[:,[0,1,2,3,4,5,6,7,11,10,12]]  
 #code 公司簡稱 法人  投信 自營商 total  收盤價 漲跌(+/-)  total_values  auth_stock con_days 1day% con_days%
 
+### plot Rep_3_Investors_price for line 
+## code , con_days%
+#print('match_row_0_9:' ,match_row.iloc[:,[0,9]])
+plot_Rep_3_Investors_price(match_row.iloc[:,[0,9]])
+
+
+
+
 for  idx in range(2,11,1) :
 
     
-    if idx ==9 :	
-         match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="green">+%s</font>' % x  if x >0  else f'<font color="red">%s</font>' % x if x < 0 else 0 )
+    #if idx ==9 :	
+    if idx in [5,7,8,9,10]:
+         #match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="green">+%s</font>' % x  if x >0  else f'<font color="red">%s</font>' % x if x < 0 else 0 )
+         match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="green">%s</font>' % x  if x <0  else f'<font color="red">+%s</font>' % x if x > 0 else 0)
          #match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="green">+%s</font>' % x if x >=5  else f'<font color="red">%dday</font>' % x if x < 0 else 0 )
     #elif  idx == 10:
     #	   match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="green">+%s</font>' % x if x >= 10 else f'<font color="red">%dday</font>' % x if x < 0 else 0 )	
     elif idx != 6:
-         match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="red">%s</font>' % x if x < 0 else str(x))
+         #match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="red">%s</font>' % x if x < 0 else str(x))
+         match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="green">%s</font>' % x if x < 0 else str(x))
+
 
 
 
@@ -465,7 +576,6 @@ if time.strftime("%H:%M:%S", time.localtime()) > mail_time :
 
     if not match_row.empty :
        body = match_row.to_html(escape=False)
-       #body = match_row.sort_values(by=['con_days']).to_html(escape=False)
        #body_color = body.replace('<td><font color="green">+5</font></td>','<td style="background-color:yellow;"><font color="green">+5</font></td>').replace('<td><font color="green">+10</font></td>','<td style="background-color:yellow;"><font color="green">+10</font></td>').replace('<td><font color="red">-5day</font></td>','<td style="background-color:#D3D3D3;"><font color="red">-5</font></td>').replace('<td><font color="red">-10day</font></td>','<td style="background-color:#D3D3D3;"><font color="red">-10</font></td>')
        #send_mail.send_email('Rep_3_Investors_%s' % date_sii ,body_color)
        send_mail.send_email('Rep_3_Investors_%s' % date_sii ,body)
