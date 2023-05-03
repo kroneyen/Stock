f6f223a79bd42550d9e61ee5c64c63e6a6b987b4
kroneyen <krone.huang@gmail.com>
2023-05-03 12:17:59 +0800

    fix bug different column of every years

--- report/stock_season_report.py
+++ report/stock_season_report.py
@@ -9,20 +9,18 @@ import line_notify
 from bs4 import BeautifulSoup
 import send_mail
 from pymongo import MongoClient
+from fake_useragent import UserAgent
+import del_png
+
+
+### del images/*.png
+del_png.del_images()
+
+
 
 
 mail_time = "18:00:00"
 #mail_time = "09:00:00"
-
-def color_red(val):
-    if val < 0:
-        color = 'red'
-    #elif val < 0:
-    #    color = 'green'
-    else:
-        color = 'block'
-    return 'color: %s' % color
-
 
 
 
@@ -95,42 +93,35 @@ def stock_season_report(year, season, yoy_up,yoy_low):
     pd.options.display.max_columns 
     pd.options.display.max_colwidth = 200 
 
+    user_agent = UserAgent()
+
     url_sii = 'https://mops.twse.com.tw/mops/web/ajax_t163sb04?encodeURIComponent=1&step=1&firstin=1&off=1&isQuery=Y&TYPEK=sii&year='+ str(year-1911)  +'&season='+ str(season)
     url_otc = 'https://mops.twse.com.tw/mops/web/ajax_t163sb04?encodeURIComponent=1&step=1&firstin=1&off=1&isQuery=Y&TYPEK=otc&year='+ str(year-1911)  +'&season='+ str(season)    
     url_list =[url_sii,url_otc]
-    #type_list=['sii','otc']    
-     
-    df_f = pd.DataFrame()
-    #u_index = 0
+    
+    df_f = pd.DataFrame() ## sii & otc
+
     for url in url_list : 
        dfs=pd.DataFrame()
-       headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_1\
-       0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36'}
-
-       r =  requests.post(url,headers=headers)
+       r =  requests.post(url ,  headers={ 'user-agent': user_agent.random })
        r.encoding = 'utf8'
        soup = BeautifulSoup(r.text, 'html.parser')
        tables = soup.find_all('table',attrs={"class": "hasBorder"})
-       for i in range(1,len(tables)+1) : 
-        df = pd.read_html(r.text,thousands=",")[i] ## 多表格取的[i]為dataframe
-        dfs = pd.concat([dfs,df],ignore_index=True) ##合併
-       dfs.columns= llist(len(dfs.columns)) ##重新編列columns
-       dfs = dfs.iloc[:,[0,1,21]]
-       dfs.columns= llist(len(dfs.columns)) ##重新編列columns 
-  
-    
-       """
-       if year == 2021 : ## 欄位不同
-          dfs = dfs.iloc[:,[0,1,21]]
-          dfs.columns= llist(len(dfs.columns)) ##重新編列columns 
-       else :
-           dfs = dfs.iloc[:,[0,1,29]]
-           dfs.columns= llist(len(dfs.columns)) ##重新編列columns        
-       """
-       dfs.columns= llist(len(dfs.columns)) ##重新編列columns
-       df_f = pd.concat([df_f,dfs],ignore_index=True) ###合併
        
-       #u_index +=1 
+
+       for i in range(1,len(tables)+1) : 
+
+         df = pd.read_html(r.text,thousands=",")[i] ## 多表格取的[i]為dataframe
+
+         df.columns= llist(len(df.columns)) ##重新編列columns
+         last_col_num = len(df.columns)-1  ## get last columns num (基本每股盈餘（元）) 17 or 21 or 29
+
+         df = df.iloc[:,[0,1,last_col_num]] ## 每年欄位不同 
+         df.columns= llist(len(df.columns)) ##重新編列columns  0 ,1 ,2
+         dfs = pd.concat([dfs,df],ignore_index=True) ##合併
+
+    
+       df_f = pd.concat([df_f,dfs],ignore_index=True) ###合併
 
        time.sleep(1)
 
@@ -144,11 +135,11 @@ def stock_season_report(year, season, yoy_up,yoy_low):
     df_f[0]= df_f[0].astype('str')
     ### filter com_lists data
     df_f =df_f[df_f[0].isin(com_lists)]
+    #print('2sec_url:',url , 'df_info:',df.info())
     df_f = df_f.sort_values(by=[2],ascending = False,ignore_index = True) ## 排序
     sst = "Q%s累計盈餘" %(season)
     #df_f.columns = ['公司代號', '公司簡稱','基本每股盈餘（元）' ]  ##定義欄位
-    df_f.columns = ['公司代號', '公司簡稱',sst ]  ##定義欄位
-
+    df_f.columns = ['公司代號', '公司名稱',sst ]  ##定義欄位
     return df_f
 
 
@@ -167,7 +158,6 @@ elif  today.month > 10 and today.month<= 12 :
 else :
   season = 4 
 
-#match_row = stock_season_report(today.year,season,'undefined','undefined')
          
 #### 累計EPS年增率
 
@@ -188,27 +178,25 @@ else :
 
 try :
 
-        #last_year = stock_season_report(today.year -1 ,season ,'undefined','undefined')  ## last year season
         last_year = stock_season_report( last_yy ,season ,'undefined','undefined')  ## last year season
 
         time.sleep(1)
 
-        #the_year = stock_season_report(today.year,season,'undefined','undefined')  ## the year season
         the_year = stock_season_report(yy ,season,'undefined','undefined')  ## the year season
 
-
 except :
+        
          
         ### if no data  get last season
         last_year = stock_season_report( last_yy ,season -1 ,'undefined','undefined')  ## last year season
 
         time.sleep(1)
 
-        the_year = stock_season_report( yy ,season -1 ,'undefined','undefined')  ## the year season 
+        the_year = stock_season_report( yy ,season -1 ,'undefined','undefined')  ## the year season
+ 
 
 
-
-s_df = the_year.merge(last_year, how='inner', on=['公司代號','公司簡稱'],suffixes=('_%s' % str(yy) , '_%s' % str(last_yy))).copy() ## merge 2021_Q3 & 2020_Q3 
+s_df = the_year.merge(last_year, how='inner', on=['公司代號','公司名稱'],suffixes=('_%s' % str(yy) , '_%s' % str(last_yy))).copy() ## merge 2021_Q3 & 2020_Q3 
 
 #### (累計EPS / 去年累計EPS - 1) * 100% 
 s_df['EPS成長%'] = (( (s_df.iloc[:,2] - (s_df.iloc[:,3]).abs() )/ (s_df.iloc[:,3]).abs() ) * 100 ).round(2) ## 計算成長% (2021- 2020) /2020 * 100%
@@ -244,9 +232,17 @@ if chk == False :
 time.sleep(1)
 
 
-for idx in list(range(2,5)) :
-    match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="red">%s</font>' % str(x) if x < 0 else str(x))
 
+
+for idx in list(range(2,5)) :
+   
+    if idx == 4 :  
+
+       match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="red">+%s</font>' % x if x > 0 else  f'<font color="green">%s</font>' % x)
+
+    else :
+
+       match_row.iloc[:,idx] = match_row.iloc[:,idx].apply(lambda  x: f'<font color="green">%s</font>' % str(x) if x < 0 else str(x))
 
 
 if time.strftime("%H:%M:%S", time.localtime()) > mail_time :
