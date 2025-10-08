8bcd1e44acd63ecb7d1a40b056d099f0cfc576eb
kroneyen <krone.huang@gmail.com>
2025-10-08 15:05:10 +0800

    upgrade python3.13.7

--- report/del_png.py
+++ report/del_png.py
@@ -1,11 +1,48 @@
-#! /usr/bin/env python3.6
+#!/usr/local/python-3.9.2/bin/python3.9 
 # -*- coding: utf-8 -*-
 import subprocess
-
+#import pexpect
+#from pexpect import popen_spawn
+import os
+import re
+import time
 
 ### del images/*.png
 
 def del_images() :
- 
-    #subprocess.run(["rm", "-rf" ,"images/*.png"])
+    """
+    mypath ='images'
+    pattern = r'\.png$'
+
+    # Deleting files matching the regex pattern
+    for root, dirs, files in os.walk(mypath):
+        for file in filter(lambda x: re.search(pattern, x), files):
+            os.remove(os.path.join(root, file))
+    """
     subprocess.run("rm -rf images/*.png" ,shell=True)
+    print('del_images is done')
+
+def del_files() :
+
+    """ 
+    mypath ='Dividend_file'
+    pattern = r'\.html$'
+
+    # Deleting files matching the regex pattern
+    for root, dirs, files in os.walk(mypath):
+        for file in filter(lambda x: re.search(pattern, x), files):
+            os.remove(os.path.join(root, file))
+    """
+    subprocess.run("rm -rf Dividend_file/*.html" ,shell=True)
+    print('del_files is done')
+
+
+
+
+if __name__ == '__main__':
+  del_images()
+
+  time.sleep(1)
+
+  #del_files()
+  
